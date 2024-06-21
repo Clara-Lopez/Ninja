104ecb513d746edd21f4e4db2d5bc67c7c3490f0
Clara <clara.lopezguerrero97@gmail.com>
2024-06-21 09:22:41 -0400

    [TAREA] sensei

--- ninja.js
+++ ninja.js
@@ -20,9 +20,33 @@ class Ninja {
     }
 }
 
+class Sensei extends Ninja {
+    constructor(nombre, salud) {
+        super(nombre, salud);
+        this.sabiduria = 10;
+    }
+
+    speakWisdom() {
+        console.log("Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses.");
+        this.drinkSake();  // Sensei también puede beber sake y aumentar su salud
+    }
+
+    // Sobreescribir el método drinkSake de Ninja
+    drinkSake() {
+        this.salud += 10;
+        console.log(`El Sensei ${this.nombre} ha bebido sake y ahora su salud es ${this.salud}`);
+    }
+}
+
 // Ejemplo de uso:
 const ninja1 = new Ninja('Hanzo', 100);
 ninja1.sayName();
 ninja1.showStats();
 ninja1.drinkSake();
 ninja1.showStats();
+
+const sensei1 = new Sensei('Kano', 200);
+sensei1.sayName();
+sensei1.showStats();
+sensei1.speakWisdom();
+sensei1.showStats();
